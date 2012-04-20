72cb1a3daadcb7ccb6e1ccb1cd572ec5398e3d5b
unknown <Administrator@Dell-Lee.(none)>
2012-04-20 14:20:42 +0800

    update to 1.0.1
    add switch on/off, fix bugs in edit mode.
    
    Signed-off-by: unknown <Administrator@Dell-Lee.(none)>

--- Dictcn.js
+++ Dictcn.js
@@ -1,43 +1,68 @@
-var KMWizDictType = 2;
 
-function OnDictcnButtonClicked() {
-	var objHtmlDocument = objWindow.CurrentDocumentHtmlDocument;
-	var objScript = objHtmlDocument.createElement('script');
-	objScript.setAttribute('src','http://dict.cn/hc/init.php');
-	objHtmlDocument.body.appendChild(objScript);
-	objHtmlDocument.body.setAttribute("wizKMDocumentModified", "1", 0);
+function DictcnButtonInitiate() {
+	var objDatabase = objApp.Database;
+	if (objDatabase.Meta("wizhelp_parm","Dictcn_FLAG") != "ON") {
+		objDatabase.Meta("wizhelp_parm","Dictcn_FLAG") = "OFF";
+	}
+	//
+	var pluginPath = objApp.GetPluginPathByScriptFileName("Dictcn.js");
+	var languangeFileName = pluginPath + "plugin.ini";
+	var buttonText = objApp.LoadStringFromFile(languangeFileName, "strDictcn");
+	buttonText += ":" + objDatabase.Meta("wizhelp_parm","Dictcn_FLAG");
+	//
+	objWindow.AddToolButton("main", "KMDictcnButton", buttonText, "", "DictcnButtonChange");   	
 }
 
-function OnDictcnButtonClicked2() {
+function DictcnButtonChange() {
+	var objDatabase = objApp.Database;
+	if (objDatabase.Meta("wizhelp_parm","Dictcn_FLAG") == "ON")	{
+		objDatabase.Meta("wizhelp_parm","Dictcn_FLAG") = "OFF";
+	}
+	else {
+		objDatabase.Meta("wizhelp_parm","Dictcn_FLAG") = "ON";
+	}
+	//
+	var pluginPath = objApp.GetPluginPathByScriptFileName("Dictcn.js");
+	var languangeFileName = pluginPath + "plugin.ini";
+	var buttonText = objApp.LoadStringFromFile(languangeFileName, "strDictcn");
+	buttonText += ":" + objDatabase.Meta("wizhelp_parm","Dictcn_FLAG");
+	//
+	objWindow.RemoveToolButton("main", "KMDictcnButton");
+	objWindow.AddToolButton("main", "KMDictcnButton", buttonText, "", "DictcnButtonChange");
+	KMCheckDictcn();
+}
+
+function DictcnAdd2Doc() {
 	var objHtmlDocument = objWindow.CurrentDocumentHtmlDocument;
 	//
-	if (!objHtmlDocument.getElementById('dict_status')) {
-		var objSpan = objHtmlDocument.createElement('span');
-		objSpan.setAttribute('id','dict_status');
-		objHtmlDocument.body.appendChild(objSpan);
-		//
-		var objScript = objHtmlDocument.createElement('script');
-		objScript.setAttribute('src','http://dict.cn/hc/');
-		objHtmlDocument.body.appendChild(objScript);
-		//
-		var objScript2 = objHtmlDocument.createElement('script');
-		objScript2.text = "dictInit();";
-		objHtmlDocument.body.appendChild(objScript2);
-		//
-		objHtmlDocument.body.setAttribute("wizKMDocumentModified", "1", 0);
-	}
+	var objScript1 = objHtmlDocument.createElement("<SCRIPT language='javascript' id='KMDictcn_Script1'></SCRIPT>");
+	objScript1.setAttribute('src','http://dict.cn/hc/');
+	objHtmlDocument.body.appendChild(objScript1);
+	//
+	var objScript2 = objHtmlDocument.createElement("<SCRIPT language='javascript' id='KMDictcn_Script2'></SCRIPT>");
+	objScript2.text = "dictInit();";
+	objHtmlDocument.body.appendChild(objScript2);
 }
 
-function InitDictcnButton() {
-    var pluginPath = objApp.GetPluginPathByScriptFileName("Dictcn.js");
-    var languangeFileName = pluginPath + "plugin.ini";
-    var buttonText = objApp.LoadStringFromFile(languangeFileName, "strDictcn");
-    if (KMWizDictType == 1) {
-    	objWindow.AddToolButton("document", "DictcnButton", buttonText, "", "OnDictcnButtonClicked");
-    }
-    else {
-    	objWindow.AddToolButton("document", "DictcnButton", buttonText, "", "OnDictcnButtonClicked2");
+// function DictcnRemoveFromDoc() {
+// 	var objHtmlDocument = objWindow.CurrentDocumentHtmlDocument;
+// 	//
+// 	var objScript1 = objHtmlDocument.getElementById('KMDictcn_Script1')
+// 	var objScript2 = objHtmlDocument.getElementById('KMDictcn_Script2')
+// 	if (objScript1) { objScript1.removeNode(true); }
+// 	if (objScript2) { objScript2.removeNode(true); }
+// 	//
+// }
+
+function KMCheckDictcn(){
+	var objDatabase = objApp.Database;
+	if (objDatabase.Meta("wizhelp_parm","Dictcn_FLAG") == "ON") {
+		DictcnAdd2Doc();
 	}
+	// else {
+	// 	DictcnRemoveFromDoc();
+	// }
 }
 
-InitDictcnButton();
+DictcnButtonInitiate();
+eventsHtmlDocumentComplete.add(KMCheckDictcn);
